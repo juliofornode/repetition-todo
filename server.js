0fe74b6a10344d8fd49cc1c448db5d0192418500
Julio <juliofornode@gmail.com>
2016-04-12 09:47:51 -0700

    working on routes and views

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 //npm modules
 var express = require('express');
 var path = require('path');
+var http = require('http');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
 
 //our own modules
 var routes = require('./routes');
@@ -15,16 +19,24 @@ var app = express();
 
 
 //4. app configuration: views, views engine
-app.set('views', path.join(__dirname, '/public'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+var port = app.get('port');
 
 
 //5. middleware definition: static, error handling, form handling
 app.use(express.static(path.join('/public')));
+app.use(methodOverride());
+app.use(logger('dev'));
+// development only
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+}
 
 
 //6. routes and request handlers
-app.get('/', routes);
+app.get('/', routes.index);
 
 
 //catch-all route
@@ -33,3 +45,6 @@ app.all('*', function(req, res) {
 });
 
 //7. app server start on host and port
+http.createServer(app).listen(port, function(req, res) {
+    console.log('the server is listening on port ', port);
+});
